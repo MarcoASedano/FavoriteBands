cefd20f30a3e5e7d9f5d19c63f9dddc4f071339b
Marco Sedano <marcoAsedano@gmail.com>
2019-01-19 10:45:28 -0800

    Added views directory with landing page ejs file

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-  res.send("Your app is up! This will be the landing page");
+  res.render("homepage");
 });
 
 app.listen(3000, function() {
-  console.log("The YelpCamp server has started!");
+  console.log("The FavoriteBands server has started!");
 });
