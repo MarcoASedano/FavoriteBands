22d9cb29e5db74eb72ad736d437810e6d381559e
Marco Sedano <marcoAsedano@gmail.com>
2019-01-19 11:44:02 -0800

    Added some temporary hardcoded test data. Listed out bands in seperate ejs homepage

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-  res.render("homepage");
+  var bands = [
+    {name: "blink-182", img: "https://s.hdnux.com/photos/10/35/03/2216160/5/920x920.jpg",wiki: "https://en.wikipedia.org/wiki/Blink-182"},
+    {name: "Green Day", img: "https://www.billboard.com/files/styles/article_main_image/public/media/green-day-wbr-press-2016-cr-frank-maddocks-billboard-1548.jpg", wiki: "https://en.wikipedia.org/wiki/Green_Day"},
+    {name: "Yellowcard", img: "http://www.janthord.com/webdevt/yellowcard/yellowcardabout.jpg", wiki: "https://en.wikipedia.org/wiki/Yellowcard"}
+  ];
+
+  res.render("homepage", {bands: bands});
 });
 
 app.listen(3000, function() {
