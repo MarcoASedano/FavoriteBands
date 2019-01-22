add7abf64febd712014285471d962ae56ed9f3a2
Marco Sedano <marcoAsedano@gmail.com>
2019-01-21 16:51:35 -0800

    set static file directory, made boostrap grid of bands

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose");
 
+// set public directory for static files
+app.use(express.static("public"));
+
+// use bodyParser to parse incoming post requests
 app.use(bodyParser.urlencoded({extended: true}));
+
+// set view engine to use ejs templates
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
   var bands = [
-    {name: "blink-182", img: "https://s.hdnux.com/photos/10/35/03/2216160/5/920x920.jpg",wiki: "https://en.wikipedia.org/wiki/Blink-182"},
+    {name: "blink-182", img: "https://images.complex.com/complex/image/upload/c_scale,w_1920/fl_lossy,pg_1,q_auto/Blink182_AH5B4305_gbvh7y.jpg",wiki: "https://en.wikipedia.org/wiki/Blink-182"},
     {name: "Green Day", img: "https://www.billboard.com/files/styles/article_main_image/public/media/green-day-wbr-press-2016-cr-frank-maddocks-billboard-1548.jpg", wiki: "https://en.wikipedia.org/wiki/Green_Day"},
     {name: "Yellowcard", img: "http://www.janthord.com/webdevt/yellowcard/yellowcardabout.jpg", wiki: "https://en.wikipedia.org/wiki/Yellowcard"},
     {name: "Sum 41", img: "https://i.scdn.co/image/34a52aea7e7e3a194df14ce5ca5632d1c09e4dfe", wiki: "https://en.wikipedia.org/wiki/Sum_41"},
