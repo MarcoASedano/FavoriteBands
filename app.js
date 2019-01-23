fd539777d049ef699bc30db46e174123647a3515
Marco Sedano <marcoAsedano@gmail.com>
2019-01-23 00:05:56 -0800

    create mongoose database and individual band page

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var express     = require("express"),
     mongoose    = require("mongoose")
     request     = require("request");
 
+//connect to mongoose server
+mongoose.connect("mongodb://localhost/favoritebands", {useNewUrlParser: true});
+
 // set public directory for static files
 app.use(express.static("public"));
 
@@ -13,26 +16,58 @@ app.use(bodyParser.urlencoded({extended: true}));
 // set view engine to use ejs templates
 app.set("view engine", "ejs");
 
-// sample data
-var bands = [
-  {name: "blink-182", img: "https://images.complex.com/complex/image/upload/c_scale,w_1920/fl_lossy,pg_1,q_auto/Blink182_AH5B4305_gbvh7y.jpg",wiki: "https://en.wikipedia.org/wiki/Blink-182"},
-  {name: "Green Day", img: "https://www.billboard.com/files/styles/article_main_image/public/media/green-day-wbr-press-2016-cr-frank-maddocks-billboard-1548.jpg", wiki: "https://en.wikipedia.org/wiki/Green_Day"},
-  {name: "Yellowcard", img: "http://www.janthord.com/webdevt/yellowcard/yellowcardabout.jpg", wiki: "https://en.wikipedia.org/wiki/Yellowcard"},
-  {name: "Sum 41", img: "https://i.scdn.co/image/34a52aea7e7e3a194df14ce5ca5632d1c09e4dfe", wiki: "https://en.wikipedia.org/wiki/Sum_41"},
-  {name: "Avenged Sevenfold", img: "https://images-na.ssl-images-amazon.com/images/G/01/music/artists/avenged_2.jpg", wiki: "https://en.wikipedia.org/wiki/Avenged_Sevenfold"},
-  {name: "Mumford & Sons", img: "https://www.clashmusic.com/sites/default/files/styles/article_feature/public/field/image/James-Minchin_MumfordSons_4736_RET_AS_FL%20copy.jpg?itok=ZTgDcWGU", wiki: "https://en.wikipedia.org/wiki/Mumford_%26_Sons"},
-  {name: "Red Hot Chili Peppers", img: "http://concreteplanet.com/wp-content/uploads/2013/08/271322_10150280648693919_2322840_o.jpg", wiki: "https://en.wikipedia.org/wiki/Red_Hot_Chili_Peppers"},
-];
+// Schema Setup
+var bandSchema = new mongoose.Schema({
+  name: String,
+  img: String,
+  wiki: String
+});
+
+var Band = mongoose.model("Band", bandSchema);
 
 app.get("/", function(req, res) {
-  res.render("home", {bands: bands});
+  res.redirect("/bands");
 });
 
-app.get("/:band", function(req, res) {
-  band = bands.find(x => x.name === req.params.band);
-  res.render("band", {band: band});
+// show all bands
+app.get("/bands", function(req, res) {
+  Band.find({}, function(err, allBands) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {bands: allBands});
+    }
+  })
 });
 
+// add new band object to database
+app.post("/bands", function(req, res) {
+  var name = req.body.name;
+  var img = req.body.img;
+  var wiki = req.body.wiki;
+  var newBand = {name: name, img: img, wiki: wiki};
+
+  Band.create(newBand, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/bands/" + name);
+    }
+  });
+});
+
+// get more information about one band
+app.get("/bands/:band", function(req, res) {
+  Band.find({name: req.params.band}, function(err, band) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("band", {band: band[0]});
+    }
+  });
+});
+
+// start server
 app.listen(3000, function() {
   console.log("The FavoriteBands server has started!");
 });
