029c8970ded294d842988b8b50baa108533e42cb
Marco Sedano <marcoAsedano@gmail.com>
2019-01-28 18:46:52 -0800

    authentication progress. add routes for login and signup

--- app.js
+++ app.js
@@ -25,6 +25,18 @@ app.use(methodOverride("_method"));
 // set view engine to use ejs templates
 app.set("view engine", "ejs");
 
+// configure PASSPORT for authentication
+app.use(require("express-session")({
+  secret: "This will be used to encrypt",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //****** ROUTES ********//
 
 app.get("/", function(req, res) {
@@ -137,6 +149,35 @@ app.post("/bands/:id/comments", function(req, res) {
     }
   });
 })
+
+// AUTHENTICATION Routes
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function() {
+      res.redirect("/bands");
+    });
+  });
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/bands",
+    failureRedirect: "/login"
+  }), function(req, res) {
+});
 
 // start server
 app.listen(3000, function() {
